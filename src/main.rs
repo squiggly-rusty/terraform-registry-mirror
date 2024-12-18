1d2713d3fec0452c87810b62a31ab947ff82f753
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-12-18 15:58:38 +0100

    add simple tracing/logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            TraceLayer::new_for_http(),
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
