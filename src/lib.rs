0ea38b01e747e8d81b087a774b12ba33e66edab1
Mattia Procopio <promat85@gmail.com>
2024-12-18 17:42:47 +0100

    First stub at storage backend

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+enum PackageKind {
+    Module,
+    Provider,
+}
+
+fn return_package_type(kind: PackageKind) -> String {
+    match kind {
+        PackageKind::Module => String::from("modules"),
+        PackageKind::Provider => String::from("providers"),
+    }
+}
+
+trait StorageBackend {
+    fn new() -> Self;
+    async fn check_package_versions(
+        &self,
+        namespace: &str,
+        kind: PackageKind,
+        package_name: &str,
+    ) -> Result<String, reqwest::Error> {
+        Ok(reqwest::get(format!(
+            "{}",
+            format_args!(
+                "https://registry.terraform.io/v1/{}/{}/{}/versions",
+                return_package_type(kind),
+                namespace,
+                package_name
+            )
+        ))
+        .await?
+        .text()
+        .await?)
+    }
+}
