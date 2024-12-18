185e1bec8c108e260b481d8f8ad0227fd9e2522d
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-12-18 17:58:51 +0100

    use the method

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, extract::Path};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -10,7 +10,7 @@ async fn main() {
 
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
+        .route(":hostname/:namespace/:package_name/index.json", get(mock_versions))
         .layer(
             TraceLayer::new_for_http(),
         );
@@ -18,4 +18,8 @@ async fn main() {
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn mock_versions(Path((hostname, namespace, package_name)): Path<(String, String, String)>) -> String {
+    return check_package_versions(namespace, PackageKind.Provider, package_name);
 }
