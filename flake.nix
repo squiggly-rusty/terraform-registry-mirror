68e41b5ab64912c33d55c2df5386e91d5692b80a
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-12-18 13:31:22 +0100

    add nixos stuff

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+      in with pkgs; {
+        devShells.default =
+          mkShell { buildInputs = [ rust-bin.stable.latest.default ]; };
+        packages.default = rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+
+          src = nix-gitignore.gitignoreSource [ "flake.nix" "flake.lock" ] ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
+}
